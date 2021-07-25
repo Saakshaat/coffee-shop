45be654cde755b77010b1cbd8e6944ada33ea354
Saakshaat <saakshaat2001@gmail.com>
2021-07-26 02:41:59 +0530

    Update orders route

--- rest-service/app/api/order.py
+++ rest-service/app/api/order.py
@@ -4,7 +4,7 @@ from schemas import order as schemas
 order_router = APIRouter()
 
 
-@order_router.get("/order/{id_}", response_model=schemas.OrderGet, status_code=200)
+@order_router.get("/orders/{id_}", response_model=schemas.OrderGet, status_code=200)
 async def get_order_status(id_: int):
     """
     Push a message for checking the current order status from the workers.
@@ -13,6 +13,6 @@ async def get_order_status(id_: int):
     return {"id": id_, "wait_time": 0}
 
 
-@order_router.post("/order", response_model=schemas.OrderGet, status_code=202)
+@order_router.post("/orders", response_model=schemas.OrderGet, status_code=202)
 async def create_order(order: schemas.OrderPost):
     return {"id": 43, "wait_time": 0}
